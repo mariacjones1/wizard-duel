5a97c621a151f71864fb07828e60b7eaf8f27716
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-02-07 10:32:01 +0000

    Validate user start input

--- run.py
+++ run.py
@@ -3,20 +3,43 @@ def start_game():
     Show game introduction and instructions to user and
     provide option to initiate game
     """
-    print("Hello and welcome to...\n")
-    print("WIZARD DUEL!\n")
-    print("Instructions:")
-    print("Test your wizarding skills against the computer.")
-    print("For each new round, you can choose to attack ('A'),")
-    print("defend ('D') or sneak ('S') by selecting the")
-    print("corresponding letter on your keyboard.")
-    print("Attacking beats sneaking, sneaking beats defending")
-    print("and defending beats attacking. Think rock, paper, scissors,")
-    print("only magical!\n")
-    print("Now that you know the rules, would you like to play?")
-    user_start = input("Y/N\n")
+    while True:
+        user_start = input("Y/N\n").upper()
+
+        try:
+            if user_start == "Y":
+                print("starting game")
+                break
+            elif user_start == "N":
+                print("Maybe next time!")
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print(f"Invalid input: {user_start}. Please type 'Y' or 'N'")
 
     return user_start
 
 
-start = start_game()
+# def main():
+#     """
+#     Run main game functions
+#     """
+#     start_game()
+#     print(start_game())
+
+
+print("Hello and welcome to...\n")
+print("WIZARD DUEL!\n")
+print("Instructions:")
+print("Test your wizarding skills against the computer.")
+print("For each new round, you can choose to attack ('A'),")
+print("defend ('D') or sneak ('S') by selecting the")
+print("corresponding letter on your keyboard.")
+print("Attacking beats sneaking, sneaking beats defending")
+print("and defending beats attacking. Think rock, paper, scissors,")
+print("only magical!\n")
+print("Now that you know the rules, would you like to play?")
+
+# main()
+start_game()
